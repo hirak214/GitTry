9b7834c242eabb8fb5c8b928e6d9f313f8cdf6e1
Hirak Desai <hirak214@gmail.com>
2020-02-26 00:04:24 +0530

    Created Class Shape

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+        Shape square = new Shape();
+        square.setShape("Square");
+        System.out.println("the shape is: " + square.getShape());
+    }
+}
---
+++ src/com/company/Shape.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Shape {
+    private String shape;
+
+    public String getShape() {
+        return shape;
+    }
+
+    public void setShape(String shape) {
+        this.shape = shape;
+    }
+}
